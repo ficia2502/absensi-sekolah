34a840936ee7b3e75fabb0957b03f88bbe567539
Moh. Rofiqi <88334099+ficilabs@users.noreply.github.com>
2025-06-10 20:24:06 +0000

    fix bug 3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,7 +17,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="/"><b>My</b>Absen</a>
+    <a href="/"><b>Vi</b>bsen</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
